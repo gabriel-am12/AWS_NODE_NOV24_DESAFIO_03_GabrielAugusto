76bd7dca52a60d2be5c686a7092fd5ebd7f80c00
gabriel acer <gabrielaug4@gmail.com>
2024-12-02 14:51:26 -0300

    refactor: change mysql to postgresql on container

--- docs/swaggerConfig.ts
+++ docs/swaggerConfig.ts
@@ -1,26 +1,32 @@
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const swaggerOptions = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'API do projeto Compass CAR para gerenciar autenticação, clientes, veículos e pedidos de forma segura e eficiente',
+      title: "API Documentation",
+      version: "1.0.0",
+      description:
+        "API do projeto Compass CAR para gerenciar autenticação, clientes, veículos e pedidos de forma segura e eficiente",
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: `http://localhost:${PORT}`,
       },
     ],
   },
-  apis: ['./src/modules/**/*.ts'], // Define onde o Swagger buscará as rotas e comentários de documentação
+  apis: ["./src/modules/**/*.ts"], // Define onde o Swagger buscará as rotas e comentários de documentação
 };
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
