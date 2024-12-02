944c603fe66acd2c9032df1ffb5af0c4c1da8ce9
gabriel acer <gabrielaug4@gmail.com>
2024-12-02 13:04:36 -0300

    refactor: config files

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,31 @@ module.exports = {
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  clearMocks: true,
+  forceExit: true,
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/src/app.ts",
+    "<rootDir>/src/@types/express.d.ts",
+    "<rootDir>/src/config",
+    "<rootDir>/src/seeds/seed.ts",
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}", // Inclui arquivos TypeScript
+    "!src/**/__tests__/**/*", // Ignora arquivos de testes na contagem de cobertura
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
 };
